930c9479d582fcbfe376f40766f6f70d7203d190
Brian Ross <rosbrian@umich.edu>
2022-11-16 08:28:52 -0500

    added mounting of drives on 353 and echoing at end of add_repo_exclusions.sh

--- add_repo_exclusions.sh
+++ add_repo_exclusions.sh
@@ -3,4 +3,5 @@ IFS=$'\n'
 
 cd
 find . -maxdepth 1 ! -name \.bashrc ! -name \.vimrc ! -name \.tmux\.conf ! -name \.functions ! -name README\.md ! -name \.ideavimrc ! -name add_repo_exclusions\.sh | sed '/^\.$/d;s/^\.\///' > .git/info/exclude
-
+echo "git status:"
+git status
